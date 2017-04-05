2f689f526df1d7bc903c663cd86358d787ed5861
Adrian Zankich <azankich@pivotal.io>
2017-04-05 12:01:31 -0700

    Create network alias on lo
    - Change the default bind address to 169.254.0.2
    
    [#142087925](https://www.pivotaltracker.com/story/show/142087925)

--- src/acceptance_tests/release_smoke_test.go
+++ src/acceptance_tests/release_smoke_test.go
@@ -19,12 +19,12 @@ var _ = Describe("Integration", func() {
 		Expect(err).NotTo(HaveOccurred())
 
 		Eventually(session, 10*time.Second).Should(gexec.Exit(0))
-		Eventually(session.Out).Should(gbytes.Say("dns.*UDP localhost:domain"))
-		Eventually(session.Out).Should(gbytes.Say("dns.*TCP localhost:domain"))
+		Eventually(session.Out).Should(gbytes.Say("dns.*UDP 169.254.0.2:domain"))
+		Eventually(session.Out).Should(gbytes.Say("dns.*TCP 169.254.0.2:domain"))
 	})
 
 	It("should respond to tcp dns queries", func() {
-		cmd := exec.Command(boshBinaryPath, []string{"ssh", "-c", "dig +tcp bosh.io @127.0.0.1"}...)
+		cmd := exec.Command(boshBinaryPath, []string{"ssh", "-c", "dig +tcp bosh.io @169.254.0.2"}...)
 		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 
@@ -32,11 +32,11 @@ var _ = Describe("Integration", func() {
 		Eventually(session.Out).Should(gbytes.Say("Got answer:"))
 		Eventually(session.Out).Should(gbytes.Say("flags: qr rd; QUERY: 1, ANSWER: 0, AUTHORITY: 0, ADDITIONAL: 0"))
 		Eventually(session.Out).Should(gbytes.Say(";bosh.io\\.\\s+IN\\s+A"))
-		Eventually(session.Out).Should(gbytes.Say("SERVER: 127.0.0.1#53"))
+		Eventually(session.Out).Should(gbytes.Say("SERVER: 169.254.0.2#53"))
 	})
 
 	It("should respond to udp dns queries", func() {
-		cmd := exec.Command(boshBinaryPath, []string{"ssh", "-c", "dig +notcp bosh.io @127.0.0.1"}...)
+		cmd := exec.Command(boshBinaryPath, []string{"ssh", "-c", "dig +notcp bosh.io @169.254.0.2"}...)
 		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 
@@ -44,6 +44,6 @@ var _ = Describe("Integration", func() {
 		Eventually(session.Out).Should(gbytes.Say("Got answer:"))
 		Eventually(session.Out).Should(gbytes.Say("flags: qr rd; QUERY: 1, ANSWER: 0, AUTHORITY: 0, ADDITIONAL: 0"))
 		Eventually(session.Out).Should(gbytes.Say(";bosh.io\\.\\s+IN\\s+A"))
-		Eventually(session.Out).Should(gbytes.Say("SERVER: 127.0.0.1#53"))
+		Eventually(session.Out).Should(gbytes.Say("SERVER: 169.254.0.2#53"))
 	})
 })
