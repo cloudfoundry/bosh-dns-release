6b2ab46203da3be740baf1a4fc72dadcdd9badea
Adrian Zankich <azankich@pivotal.io>
2017-04-06 10:53:07 -0700

    Use healthcheck.bosh-dns. for dns server health check
    [#140699353](https://www.pivotaltracker.com/story/show/140699353)
    
    Signed-off-by: Mario Manno <mario.manno@gmail.com>

--- src/acceptance_tests/release_smoke_test.go
+++ src/acceptance_tests/release_smoke_test.go
@@ -24,26 +24,26 @@ var _ = Describe("Integration", func() {
 	})
 
 	It("should respond to tcp dns queries", func() {
-		cmd := exec.Command(boshBinaryPath, []string{"ssh", "-c", "dig +tcp bosh.io @169.254.0.2"}...)
+		cmd := exec.Command(boshBinaryPath, []string{"ssh", "-c", "dig +tcp healthcheck.bosh-dns. @169.254.0.2"}...)
 		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 
 		Eventually(session, 10*time.Second).Should(gexec.Exit(0))
 		Eventually(session.Out).Should(gbytes.Say("Got answer:"))
 		Eventually(session.Out).Should(gbytes.Say("flags: qr rd; QUERY: 1, ANSWER: 0, AUTHORITY: 0, ADDITIONAL: 0"))
-		Eventually(session.Out).Should(gbytes.Say(";bosh.io\\.\\s+IN\\s+A"))
+		Eventually(session.Out).Should(gbytes.Say(";healthcheck\\.bosh-dns\\.\\s+IN\\s+A"))
 		Eventually(session.Out).Should(gbytes.Say("SERVER: 169.254.0.2#53"))
 	})
 
 	It("should respond to udp dns queries", func() {
-		cmd := exec.Command(boshBinaryPath, []string{"ssh", "-c", "dig +notcp bosh.io @169.254.0.2"}...)
+		cmd := exec.Command(boshBinaryPath, []string{"ssh", "-c", "dig +notcp healthcheck.bosh-dns. @169.254.0.2"}...)
 		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 
 		Eventually(session, 10*time.Second).Should(gexec.Exit(0))
 		Eventually(session.Out).Should(gbytes.Say("Got answer:"))
 		Eventually(session.Out).Should(gbytes.Say("flags: qr rd; QUERY: 1, ANSWER: 0, AUTHORITY: 0, ADDITIONAL: 0"))
-		Eventually(session.Out).Should(gbytes.Say(";bosh.io\\.\\s+IN\\s+A"))
+		Eventually(session.Out).Should(gbytes.Say(";healthcheck\\.bosh-dns\\.\\s+IN\\s+A"))
 		Eventually(session.Out).Should(gbytes.Say("SERVER: 169.254.0.2#53"))
 	})
 })
