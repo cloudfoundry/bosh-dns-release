11d88d88797308ea4c9826d27e4ff6d1b4308fa3
Adrian Zankich <azankich@pivotal.io>
2017-04-11 16:50:05 -0700

    Add records_file job spec property
    [#140701789](https://www.pivotaltracker.com/story/show/140701789)
    
    Signed-off-by: Tom Viehman <tviehman@pivotal.io>

--- src/acceptance_tests/release_smoke_test.go
+++ src/acceptance_tests/release_smoke_test.go
@@ -1,6 +1,9 @@
 package acceptance_test
 
 import (
+	"fmt"
+	"strings"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -56,6 +59,32 @@ var _ = Describe("Integration", func() {
 		Eventually(session.Out).Should(gbytes.Say("Got answer:"))
 		Eventually(session.Out).Should(gbytes.Say("flags: qr rd ra; QUERY: 1, ANSWER: 2, AUTHORITY: 0, ADDITIONAL: 1"))
 		Eventually(session.Out).Should(gbytes.Say("pivotal\\.io\\.\\s+\\d+\\s+IN\\s+A\\s+\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}"))
+		Eventually(session.Out).Should(gbytes.Say("SERVER: 169.254.0.2#53"))
+	})
+
+	PIt("returns records for bosh instances", func() {
+		cmd := exec.Command(boshBinaryPath, []string{"instances", "--column", "Instance"}...)
+		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(session, 10*time.Second).Should(gexec.Exit(0))
+
+		instanceID := strings.Split(strings.TrimSpace(string(session.Out.Contents())), "/")[1]
+
+		cmd = exec.Command(boshBinaryPath, []string{"instances", "--column", "IPs"}...)
+		session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(session, 10*time.Second).Should(gexec.Exit(0))
+
+		ip := strings.TrimSpace(string(session.Out.Contents()))
+
+		cmd = exec.Command(boshBinaryPath, []string{"ssh", "-c", fmt.Sprintf("dig -t A %s.dns.default.bosh-dns.bosh @169.254.0.2", instanceID)}...)
+		session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+
+		Eventually(session, 10*time.Second).Should(gexec.Exit(0))
+		Eventually(session.Out).Should(gbytes.Say("Got answer:"))
+		Eventually(session.Out).Should(gbytes.Say("flags: qr aa rd; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 0"))
+		Eventually(session.Out).Should(gbytes.Say("%s\\.dns\\.default\\.bosh-dns\\.bosh\\.\\s+0\\s+IN\\s+A\\s+%s", instanceID, ip))
 		Eventually(session.Out).Should(gbytes.Say("SERVER: 169.254.0.2#53"))
 	})
 })
