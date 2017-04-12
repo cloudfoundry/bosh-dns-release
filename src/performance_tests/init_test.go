1394a6d63513c7b7d871747a2c9bc356861de544
Dennis Leon <dleon@pivotal.io>
2017-04-11 17:03:10 -0700

    Split dns performance tests to display cpu/mem/time measurements
    - median and errors are seperate tests
    
    [#142360153](https://www.pivotaltracker.com/story/show/142360153)
    
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>

--- src/performance_tests/init_test.go
+++ src/performance_tests/init_test.go
@@ -4,10 +4,31 @@ import  (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/cloudfoundry/gosigar"
 	"testing"
+	"strings"
 )
 
 func TestAcceptance(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "performance")
+}
+
+func GetPidFor(processName string) (int, bool) {
+	pids := sigar.ProcList{}
+	pids.Get()
+
+	for _, pid := range pids.List {
+		state := sigar.ProcState{}
+
+		if err := state.Get(pid); err != nil {
+			continue
+		}
+
+		if strings.Contains(state.Name, processName) {
+			return pid, true
+		}
+	}
+
+	return -1, false
 }
--- src/performance_tests/performance_test.go
+++ src/performance_tests/performance_test.go
@@ -1,13 +1,120 @@
 package performance_test
 
 import (
+	"github.com/cloudfoundry/gosigar"
 	"github.com/miekg/dns"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"sort"
 	"sync"
 	"time"
-	"sort"
+	"fmt"
 )
+
+type PerformanceTestInfo struct {
+	MedianRequestTime         time.Duration
+	ErrorCount                int
+	FailedRequestRCodesCounts map[int]int
+	MaxRuntime                time.Duration
+}
+
+var _ = Describe("Performance", func() {
+	var maxDnsRequestsPerMin int
+	var info PerformanceTestInfo
+	var flowSignal chan bool
+	var wg *sync.WaitGroup
+	var finishedDnsRequestsSignal chan struct{}
+	var result chan DnsResult
+
+	var dnsServerPid int
+
+	Context("420 req / min", func() {
+		BeforeEach(func() {
+			var found bool
+			dnsServerPid, found = GetPidFor("dnsserver")
+			Expect(found).To(BeTrue())
+
+			maxDnsRequestsPerMin = 420
+			info = PerformanceTestInfo{}
+
+			flowSignal = createFlowSignal(10)
+			wg, finishedDnsRequestsSignal = setupWaitGroupWithSignaler(maxDnsRequestsPerMin)
+			result = make(chan DnsResult, maxDnsRequestsPerMin*2)
+		})
+
+		Measure("should handle with less than 0.797ms median", func(b Benchmarker) {
+			concreteSigar := sigar.ConcreteSigar{}
+			cpuChannel, stopCpuChannel := concreteSigar.CollectCpuStats(100 * time.Millisecond)
+
+			b.Time("dns queries", func() {
+				for i := 0; i < maxDnsRequestsPerMin; i++ {
+					go MakeDnsRequest(GooglePicker{}, flowSignal, result, wg)
+				}
+				<-finishedDnsRequestsSignal
+				close(stopCpuChannel)
+			})
+
+			mem := sigar.ProcMem{}
+			if err := mem.Get(dnsServerPid); err == nil {
+				b.RecordValue("DNS Server Memory Usage (in Mb)", float64(mem.Resident/1024/1024))
+			}
+
+			cpuResult := <- cpuChannel
+			b.RecordValue("Total CPU Usage (%)", (float64(cpuResult.User + cpuResult.Sys) / float64(cpuResult.Total())) * 100)
+		}, 5)
+
+		It("never errors", func() {
+			var resultSummary map[int]*DnsResult
+			for i := 0; i < maxDnsRequestsPerMin; i++ {
+				go MakeDnsRequest(GooglePicker{}, flowSignal, result, wg)
+			}
+			<-finishedDnsRequestsSignal
+
+			resultSummary = buildResultSummarySync(result)
+
+			for _, summary := range resultSummary {
+				Expect(summary.Error).ToNot(HaveOccurred())
+				Expect(summary.RCode).To(Equal(dns.RcodeSuccess))
+			}
+		})
+
+		It("handles DNS responses quickly", func() {
+			startTime := time.Now()
+			var resultSummary map[int]*DnsResult
+			for i := 0; i < maxDnsRequestsPerMin; i++ {
+				go MakeDnsRequest(GooglePicker{}, flowSignal, result, wg)
+			}
+			<-finishedDnsRequestsSignal
+			endTime := time.Now()
+
+			resultSummary = buildResultSummarySync(result)
+
+			resultTimes := []int{}
+			for _, summary := range resultSummary {
+				resultTimes = append(resultTimes, int(summary.EndTime.Sub(summary.StartTime)))
+			}
+
+			sort.Ints([]int(resultTimes))
+			median := (time.Duration(resultTimes[209]) + time.Duration(resultTimes[210])) / 2
+
+			Expect(endTime).Should(BeTemporally("<", startTime.Add(1*time.Minute)))
+			Expect(median).To(BeNumerically("<", 797*time.Microsecond))
+		})
+	})
+})
+
+func setupWaitGroupWithSignaler(maxDnsRequests int) (*sync.WaitGroup, chan struct{}) {
+	wg := &sync.WaitGroup{}
+	wg.Add(maxDnsRequests)
+	finishedDnsRequests := make(chan struct{})
+
+	go func() {
+		wg.Wait()
+		close(finishedDnsRequests)
+	}()
+
+	return wg, finishedDnsRequests
+}
 
 type ZonePicker interface {
 	NextZone() string
@@ -18,19 +125,22 @@ type DnsResult struct {
 	RCode     int
 	StartTime time.Time
 	EndTime   time.Time
-	Error error
+	Error     error
 }
 
-type GooglePicker struct {}
+type GooglePicker struct{}
 
 func (GooglePicker) NextZone() string {
 	return "google.com."
 }
 
-func initializeFlow(flow chan bool) {
+func createFlowSignal(goRoutineSize int) chan bool {
+	flow := make(chan bool, goRoutineSize)
 	for i := 0; i < 10; i++ {
 		flow <- true
 	}
+
+	return flow
 }
 
 func MakeDnsRequest(picker ZonePicker, flow chan bool, result chan DnsResult, wg *sync.WaitGroup) error {
@@ -38,6 +148,7 @@ func MakeDnsRequest(picker ZonePicker, flow chan bool, result chan DnsResult, wg
 		flow <- true
 		wg.Done()
 	}()
+
 	<-flow
 	zone := picker.NextZone()
 	c := new(dns.Client)
@@ -55,61 +166,19 @@ func MakeDnsRequest(picker ZonePicker, flow chan bool, result chan DnsResult, wg
 	return nil
 }
 
-func buildResultSummary(result chan DnsResult, finishedDnsRequests chan struct{}) map[int]*DnsResult {
+func buildResultSummarySync(result chan DnsResult) map[int]*DnsResult {
 	resultSummary := make(map[int]*DnsResult)
-	for {
-		select {
-		case r := <-result:
-			if _, found := resultSummary[r.Id]; found {
-				dnsResult := resultSummary[r.Id]
-				dnsResult.EndTime = r.EndTime
-				dnsResult.RCode = r.RCode
-			} else {
-				resultSummary[r.Id] = &r
-			}
-		case <-finishedDnsRequests:
-			return resultSummary
+	close(result)
+
+	for r := range result {
+		if _, found := resultSummary[r.Id]; found {
+			dnsResult := resultSummary[r.Id]
+			dnsResult.EndTime = r.EndTime
+			dnsResult.RCode = r.RCode
+		} else {
+			resultSummary[r.Id] = &r
 		}
 	}
+
+	return resultSummary
 }
-
-var _ = Describe("Performance", func() {
-	It("should handle 420 req / min with less than 0.797ms median", func() {
-		result := make(chan DnsResult)
-		finishedDnsRequests := make(chan struct{})
-		wg := &sync.WaitGroup{}
-		maxDnsRequests := 420
-		goRoutineSize := 10
-		flow := make(chan bool, goRoutineSize)
-
-		initializeFlow(flow)
-
-		startTime := time.Now()
-		wg.Add(maxDnsRequests)
-		for i := 0; i < maxDnsRequests; i++ {
-			go MakeDnsRequest(GooglePicker{}, flow, result, wg)
-		}
-
-		go func() {
-			wg.Wait()
-			close(finishedDnsRequests)
-		}()
-
-		resultSummary := buildResultSummary(result, finishedDnsRequests)
-		endTime := time.Now()
-		resultTimes := []int{}
-
-		for _, summary := range resultSummary {
-			Expect(summary.Error).ToNot(HaveOccurred())
-			Expect(summary.RCode).To(Equal(dns.RcodeSuccess))
-			resultTimes = append(resultTimes, int(summary.EndTime.Sub(summary.StartTime)))
-		}
-
-		sort.Ints([]int(resultTimes))
-		median := (time.Duration(resultTimes[209]) + time.Duration(resultTimes[210])) / 2
-
-		Expect(endTime).To(BeTemporally("<", startTime.Add(1*time.Minute)))
-		Expect(median).To(BeNumerically("<", 797*time.Microsecond))
-	})
-})
-
