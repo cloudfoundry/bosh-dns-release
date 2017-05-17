4d73ec27f738414a6f5707322f8b312e4897572a
Michael Maximilien <maxim@us.ibm.com>
2017-05-17 11:28:59 -0700

    Script for running acceptance test for Linux
    [#144498195](https://www.pivotaltracker.com/story/show/144498195)
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

---
+++ bin/test-acceptance.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+fly -t production login
+
+fly -t production execute -x --privileged --config=./ci/tasks/test-acceptance.yml --inputs-from=dns-release/test-acceptance --input=dns-release=$DIR/../
