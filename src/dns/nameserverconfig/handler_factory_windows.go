febdd47327fb1624cdaf1b91096c139cafb7e57a
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2017-05-18 09:42:20 -0700

    Fix windows HandlerFactory signature
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- src/dns/nameserverconfig/handler_factory_windows.go
+++ src/dns/nameserverconfig/handler_factory_windows.go
@@ -3,12 +3,11 @@
 package main
 
 import (
-	"code.cloudfoundry.org/clock"
 	boshlog "github.com/cloudfoundry/bosh-utils/logger"
 	boshsys "github.com/cloudfoundry/bosh-utils/system"
 	"github.com/cloudfoundry/dns-release/src/dns/nameserverconfig/handler"
 )
 
-func HandlerFactory(address string, _ clock.Clock, _ boshlog.Logger, runner boshsys.CmdRunner) handler.Handler {
+func HandlerFactory(address string, _ boshlog.Logger, runner boshsys.CmdRunner) handler.Handler {
 	return handler.NewWindowsHandler(address, runner)
 }
