ec12d5cfa5cc1c2a84762fadf9b85a1e5c9bcbab
Luan Santos <lsantos@pivotal.io>
2017-07-06 08:44:23 -0700

    fix bad ops file path

--- ci/tasks/test-acceptance-windows-nameserver-disabled.sh
+++ ci/tasks/test-acceptance-windows-nameserver-disabled.sh
@@ -13,7 +13,7 @@ bosh -n upload-stemcell $ROOT_DIR/bosh-candidate-stemcell-windows/*.tgz
 export BOSH_DEPLOYMENT=bosh-dns-windows-acceptance
 
 bosh -n deploy --recreate $ROOT_DIR/dns-release/src/bosh-dns/test_yml_assets/windows-acceptance-manifest.yml \
-    -o $ROOT_DIR/dns-release/bosh-dns/src/acceptance_tests/windows/disable_nameserver_override/manifest-ops.yml \
+    -o $ROOT_DIR/dns-release/src/bosh-dns/acceptance_tests/windows/disable_nameserver_override/manifest-ops.yml \
     --vars-store dns-creds.yml
 
 bosh run-errand acceptance-tests-windows
