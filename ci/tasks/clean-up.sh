c28f06ec0f2ff00e9493bb7b25d25386e51e469a
Tom Viehman <tviehman@pivotal.io>
2017-07-21 08:29:18 -0700

    do not delete deployment after windows acceptance tests
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- ci/tasks/clean-up.sh
+++ ci/tasks/clean-up.sh
@@ -8,5 +8,3 @@ BBL_STATE_DIR=$ROOT_DIR/envs/$ENV_NAME
 source $BBL_STATE_DIR/.envrc
 
 bosh -n clean-up --all
-
-bosh deployments | awk '{ print $1 }' | xargs -I name bosh -n -d name delete-deployment
