36916669304f1b3f2317e2b6077375908492df32
Difan Zhao <dzhao@pivotal.io>
2017-09-19 08:43:06 -0700

    [#150416524] Run test for coredns fork in CI
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

---
+++ ci/tasks/test-cache.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -exu
+
+ROOT_DIR=$PWD
+
+export GOPATH=$(mktemp -d)
+export COREDNS_PATH=$GOPATH/src/github.com/coredns/coredns
+mkdir -p $GOPATH/src/github.com/coredns/coredns
+cp -r coredns/* $COREDNS_PATH
+
+cd $COREDNS_PATH
+go test ./middleware/cache/...
+
