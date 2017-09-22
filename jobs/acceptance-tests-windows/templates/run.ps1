210ed22d01785e375b00f07de46a64bd55283eb5
Zachary Gershman <zgershman@pivotal.io>
2017-09-22 14:46:23 -0700

    Vendor golang 1.8.3 to dns
    [#150896170](https://www.pivotaltracker.com/story/show/150896170)
    
    Signed-off-by: Alton Fong <altonf@vmware.com>

--- jobs/acceptance-tests-windows/templates/run.ps1
+++ jobs/acceptance-tests-windows/templates/run.ps1
@@ -1,10 +1,11 @@
-﻿$env:GOROOT="C:\var\vcap\packages\golang-windows\go"
-$env:GOPATH="C:\var\vcap\packages\acceptance-tests-windows"
-$env:PATH="${env:GOPATH}\bin;${env:GOROOT}\bin;${env:PATH}"
+﻿. C:\var\vcap\packages\golang-1.8-windows\bosh\runtime.ps1
+
+New-Item -Path ${env:GOPATH}\src -ItemType directory
+Copy-Item -recurse "C:\var\vcap\packages\acceptance-tests-windows\src\bosh-dns" "$env:GOPATH\src\bosh-dns"
 
 go.exe install bosh-dns/vendor/github.com/onsi/ginkgo/ginkgo
 
-Push-Location "C:\var\vcap\packages\acceptance-tests-windows\src\bosh-dns\acceptance_tests\windows"
+Push-Location "${env:GOPATH}\src\bosh-dns\acceptance_tests\windows"
 
 ginkgo -randomizeAllSpecs -randomizeSuites -race <%= p('suites') %>
 
