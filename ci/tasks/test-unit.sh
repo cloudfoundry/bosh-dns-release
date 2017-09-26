99450b5f07e227b5cab147d263e9732b571220f3
Zachary Gershman <zgershman@pivotal.io>
2017-09-26 10:00:27 -0700

    simplifies unit-test script
    - now that packages are vendored we no longer have a need to sync
    blobs or use the built in golang.
    - golang now resides in the docker-image that is specifically built
    for unit tests.
    - taking a slight risk that there may be a golang incompatiblity but
    we are unconcerned for now.
    
    [#151448774](https://www.pivotaltracker.com/story/show/151448774)
    
    Signed-off-by: Danny Berger <dberger@pivotal.io>

--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -2,26 +2,8 @@
 
 set -exu
 
-apt-get update
-apt-get install -y wget
-
-wget -q -c "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.1-linux-amd64" -O /usr/local/bin/bosh
-echo "f23c3aecec999cfda93a3123ccc724f09856a5c9  /usr/local/bin/bosh" | shasum -c -
-
-chmod +x /usr/local/bin/bosh
-
-export BOSH_INSTALL_TARGET=/usr/local/golang
-mkdir -p $BOSH_INSTALL_TARGET
-
 export GOROOT=/usr/local/golang
-export PATH=$BOSH_INSTALL_TARGET/bin:$PATH
 
 pushd bosh-dns-release/
-  bosh sync-blobs
-
-  pushd blobs
-    ../packages/golang/packaging
-  popd
-
   scripts/test-unit
 popd
