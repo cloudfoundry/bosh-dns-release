1be64bf5613bf50f8a9c899bf7ff2f5fd6e667f4
Danny Berger <dberger@pivotal.io>
2017-09-28 15:29:42 -0700

    Fix brats pipeline and ensure bosh-src has all commits

--- ci/tasks/prepare-brats.sh
+++ ci/tasks/prepare-brats.sh
@@ -2,9 +2,14 @@
 
 set -eux
 
-git clone bosh-src bosh-src-output
-
 pinned_version=$(tar -xOzf bosh-release/*.tgz release.MF | grep commit_hash | awk '{ print $2 }' | tr -d '"')
+
+pushd bosh-src
+  # in case $pinned_version commit was made to a release branch instead of master
+  git fetch --quiet origin refs/heads/*:refs/remotes/origin/*
+popd
+
+git clone bosh-src bosh-src-output
 
 cd bosh-src-output
 
