a3b23b37d2b16fb93ddcffcb84ea47d1f52a9474
David Ansari <david.ansari@gmx.de>
2017-11-08 12:05:42 -0800

    Check in threshold definitions
    [#152263049](https://www.pivotaltracker.com/story/show/152263049)
    
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

---
+++ src/bosh-dns/performance_tests/thresholds_windows_test.go
@@ -0,0 +1,59 @@
+package performance_test
+
+import "time"
+
+func healthVitalsThresholds() VitalsThresholds {
+	return VitalsThresholds{
+		CPUPct99: 65,
+		MemPct99: 20,
+		MemMax:   25,
+	}
+}
+
+func healthTimeThresholds() TimeThresholds {
+	return TimeThresholds{
+		Med:   16 * time.Millisecond,
+		Pct90: 17 * time.Millisecond,
+		Pct95: 20 * time.Millisecond,
+	}
+}
+
+func prodLikeVitalsThresholds() VitalsThresholds {
+	return VitalsThresholds{
+		CPUPct99: 5,
+		MemPct99: 18,
+		MemMax:   20,
+	}
+}
+
+func localZonesVitalsThresholds() VitalsThresholds {
+	return VitalsThresholds{
+		CPUPct99: 14,
+		MemPct99: 36,
+		MemMax:   40,
+	}
+}
+
+func localZonesTimeThresholds() TimeThresholds {
+	return TimeThresholds{
+		Med:   3 * time.Millisecond,
+		Pct90: 4 * time.Millisecond,
+		Pct95: 5 * time.Millisecond,
+	}
+}
+
+func upcheckVitalsThresholds() VitalsThresholds {
+	return VitalsThresholds{
+		CPUPct99: 1,
+		MemPct99: 17,
+		MemMax:   20,
+	}
+}
+
+func upcheckTimeThresholds() TimeThresholds {
+	return TimeThresholds{
+		Med:   2 * time.Millisecond,
+		Pct90: 2 * time.Millisecond,
+		Pct95: 3 * time.Millisecond,
+	}
+}
