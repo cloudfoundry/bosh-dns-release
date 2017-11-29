6d486aa906120ecfae6006a6e1fd270692c9dfd9
Difan Zhao <dzhao@pivotal.io>
2017-11-29 12:11:44 -0800

    Print less.
    [#144043809](https://www.pivotaltracker.com/story/show/144043809)
    
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- ci/tasks/test-stress.sh
+++ ci/tasks/test-stress.sh
@@ -10,7 +10,9 @@ tasks_directory=$(dirname $0)
 scripts_directory=${tasks_directory}/test-stress
 cd ${scripts_directory}
 
+set +x
 eval "$(bbl print-env --state-dir=$BBL_STATE_DIR)"
+set -x
 
 deployments="${DEPLOYMENTS_OF_100:=10}"
 
@@ -24,11 +26,13 @@ pushd inner-bosh-deployment
   ./deploy-director.sh
 popd
 
+set +x
 # target inner bosh
 export BOSH_CA_CERT="$(bosh int ${STATE_DIR}/vars-store.yml --path /director_ssl/ca)"
 export BOSH_CLIENT="admin"
 export BOSH_CLIENT_SECRET="$(bosh int ${STATE_DIR}/vars-store.yml --path /admin_password)"
 export BOSH_ENVIRONMENT="https://$(bosh int inner-bosh-deployment/vars.yml --path /internal_ip):25555"
+set -x
 
   pushd inner-bosh-workspace
     # 3. 10x Deploy large bosh-dns deployment to inner director
@@ -38,8 +42,10 @@ export BOSH_ENVIRONMENT="https://$(bosh int inner-bosh-deployment/vars.yml --pat
     ./check-dns.sh $deployments
   popd
 
+set +x
 # retarget outer bosh
 eval "$(bbl print-env --state-dir=$BBL_STATE_DIR)"
+set -x
 
 # skip teardown of docker container vms since they'll be torn down with the host
 
