c8f92c65e5244e6a8f81258a82e7b5216c4c2a9a
Joshua Aresty <joshua.aresty@emc.com>
2017-11-29 14:06:19 -0800

    make-cloud-configable was duplicate with bosh-dev
    [#144043809](https://www.pivotaltracker.com/story/show/144043809)
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- ci/tasks/test-stress/inner-bosh-deployment/deploy-director.sh
+++ ci/tasks/test-stress/inner-bosh-deployment/deploy-director.sh
@@ -7,11 +7,10 @@ bosh -n -d bosh deploy \
   --vars-store ${STATE_DIR}/vars-store.yml \
   --vars-file  ./vars.yml \
   -o $BOSH_DEPLOYMENT_REPO/misc/bosh-dev.yml \
+  -o ./ops/add-docker-cpi-release.yml \
+  -o $BOSH_DEPLOYMENT_REPO/uaa.yml \
   -o $BOSH_DEPLOYMENT_REPO/jumpbox-user.yml \
   -o $BOSH_DEPLOYMENT_REPO/local-dns.yml \
-  -o $BOSH_DEPLOYMENT_REPO/uaa.yml \
   -o $BOSH_DEPLOYMENT_REPO/credhub.yml \
-  -o ./ops/add-docker-cpi-release.yml \
-  -o ./ops/make-cloud-configable.yml \
   -o ./ops/configure-max-threads.yml \
   -v docker_cpi_release_src_path=$BOSH_DOCKER_CPI_RELEASE_REPO
