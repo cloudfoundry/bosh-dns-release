e37ac6d3e4696c06b408c51561b145dcd0f61d41
Josh Russett <jrussett@pivotal.io>
2017-12-12 11:52:24 -0800

    Add WINDOWS_OS_VERSION variable to the nameserver-disabled tests.
    [#153520141](https://www.pivotaltracker.com/story/show/153520141)
    
    Signed-off-by: Dale Wick <dwick@pivotal.io>

--- ci/tasks/test-acceptance-windows-nameserver-disabled.sh
+++ ci/tasks/test-acceptance-windows-nameserver-disabled.sh
@@ -13,6 +13,7 @@ export BOSH_DEPLOYMENT=bosh-dns-windows-acceptance
 
 bosh -n deploy --recreate $ROOT_DIR/bosh-dns-release/src/bosh-dns/test_yml_assets/windows-acceptance-manifest.yml \
     -o $ROOT_DIR/bosh-dns-release/src/bosh-dns/acceptance_tests/windows/disable_nameserver_override/manifest-ops.yml \
+    -v windows_stemcell=$WINDOWS_OS_VERSION \
     --vars-store dns-creds.yml
 
 bosh run-errand acceptance-tests-windows --keep-alive
