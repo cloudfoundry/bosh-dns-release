be594712bf8f8790a3f52fccee40a84ee8c828b9
Tom Viehman <tviehman@pivotal.io>
2018-01-03 14:00:51 -0800

    bump minor version instead of patch
    - this allows us to build off branches for backports
    - also, a little sugar in configure.sh
    
    [#154029803](https://www.pivotaltracker.com/story/show/154029803)
    
    Signed-off-by: J Evelyn <jquintana@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -ex
+  lpass status
+set +ex
+
 this_dir=$(dirname $0)
 
 fly -t ${CONCOURSE_TARGET:-production} sp -p bosh-dns-release -c $this_dir/pipeline.yml --load-vars-from <(lpass show 'dns-release pipeline vars' --notes)
