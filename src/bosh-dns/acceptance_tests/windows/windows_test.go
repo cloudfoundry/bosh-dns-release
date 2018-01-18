afd06b4dd97a18f58aaeec1d3331aabdcb02075f
Sunjay Bhatia <sbhatia@pivotal.io>
2018-01-18 16:35:18 -0500

    Extend timeout in windows acceptance tests that use powershell

--- src/bosh-dns/acceptance_tests/windows/windows_test.go
+++ src/bosh-dns/acceptance_tests/windows/windows_test.go
@@ -73,7 +73,7 @@ var _ = Describe("windows tests", func() {
 		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 
-		Eventually(session).Should(gexec.Exit(0))
+		Eventually(session, 10*time.Second).Should(gexec.Exit(0))
 
 		Expect(session.Out).To(Say("0"))
 	})
@@ -83,7 +83,7 @@ var _ = Describe("windows tests", func() {
 		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 
-		Eventually(session).Should(gexec.Exit(0))
+		Eventually(session, 10*time.Second).Should(gexec.Exit(0))
 
 		Expect(session.Out).To(Say("0"))
 	})
