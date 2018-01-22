7e0c1d31831930458bd693440146b26fb7c8ac90
Joshua Aresty <joshua.aresty@emc.com>
2018-01-22 15:17:22 -0800

    Set up a pipeline for testing 0.1.x DNS release
    [#154448063](https://www.pivotaltracker.com/story/show/154448063)
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-fly -t production sp -p bosh-dns-release -c ./ci/pipeline.yml --load-vars-from <(lpass show 'dns-release pipeline vars' --notes)
+fly -t production sp -p bosh-dns-release:0.1.x -c ./ci/pipeline.yml --load-vars-from <(lpass show 'dns-release pipeline vars' --notes)
