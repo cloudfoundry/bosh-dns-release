b62b0d0e56db402a915fd33d269887f4e6f839e7
Chris Dutra <cdutra@pivotal.io>
2018-02-21 10:40:04 -0800

    Remove unnecessary scripts
    [#155295414](https://www.pivotaltracker.com/story/show/155295414)

--- ci/tasks/test-stress/deploy-inner-bosh.sh
+++ ci/tasks/test-stress/deploy-inner-bosh.sh
@@ -24,7 +24,17 @@ pushd ${scripts_directory}
 
   # 1. Deploy docker hosts to outer director
   pushd docker-hosts-deployment
-    ./deploy-docker.sh
+    bbl --state-dir=$BBL_STATE_DIR cloud-config | bosh -n update-cloud-config - \
+      -o docker-addressable-zones-template.yml \
+      -v jumpbox_table_id=$(bosh int $BBL_STATE_DIR/vars/terraform.tfstate --path /modules/0/resources/aws_route_table.bosh_route_table/primary/id) \
+      -v default_table_id=$(bosh int $BBL_STATE_DIR/vars/terraform.tfstate --path /modules/0/resources/aws_route_table.internal_route_table/primary/id)
+
+    bosh upload-stemcell https://bosh.io/d/stemcells/bosh-aws-xen-hvm-ubuntu-trusty-go_agent
+
+    bosh -n deploy -d docker ./deployments/docker.yml \
+      -l ./vars/docker-vars.yml \
+      --vars-store=${state_dir}/docker-vars-store.yml
+
   popd
 
   # 2. Deploy inner director
