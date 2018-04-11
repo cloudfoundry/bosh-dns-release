1abbe4271f277e97fc46b6300d35d37e1a3f64ce
Luan Santos <lsantos@pivotal.io>
2018-04-11 16:14:49 -0700

    Autobump bosh-dns golang dependencies
    [#156711913](https://www.pivotaltracker.com/story/show/156711913)
    
    Signed-off-by: Josh Russett <jrussett@pivotal.io>

---
+++ ci/tasks/bump-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+git clone bosh-dns-release bumped-bosh-dns-release
+
+cd bumped-bosh-dns-release/src/bosh-dns
+
+dep ensure -v -update
+
+git status
+git add vendor Gopkg.lock
+git commit -m "Update vendored dependencies"
