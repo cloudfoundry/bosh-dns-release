15267c5ab6c4ebb39a53aef4f54a9b3116312164
Chris Dutra <cdutra@pivotal.io>
2018-05-07 10:36:06 -0700

    Add delivery job to auto deliver finished stories

--- ci/configure.sh
+++ ci/configure.sh
@@ -6,4 +6,9 @@ set +ex
 
 this_dir=$(dirname $0)
 
-fly -t ${CONCOURSE_TARGET:-production} sp -p bosh-dns-release -c $this_dir/pipeline.yml --load-vars-from <(lpass show 'dns-release pipeline vars' --notes)
+fly -t ${CONCOURSE_TARGET:-production} \
+  sp -p bosh-dns-release \
+  -c $this_dir/pipeline.yml \
+  -l <(lpass show --notes 'dns-release pipeline vars') \
+  -l <(lpass show --notes 'tracker-bot-story-delivery') \
+  -v "tracker_project_id=2139998"
