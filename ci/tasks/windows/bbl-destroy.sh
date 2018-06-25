172ba4f784dd2a01548b94425765b5162054c26f
Luan Santos <lsantos@pivotal.io>
2018-06-25 14:10:14 -0700

    Fix unbalanced quoting in bbl-destory script
    [#155264563](https://www.pivotaltracker.com/story/show/155264563)

--- ci/tasks/windows/bbl-destroy.sh
+++ ci/tasks/windows/bbl-destroy.sh
@@ -6,7 +6,7 @@ main() {
   source "$PWD/bosh-dns-release/ci/assets/utils.sh"
   local output_dir="$PWD/envs-output/"
   local bbl_state_env_repo_dir=$PWD/envs
-  trap "commit_bbl_state_dir ${bbl_state_env_repo_dir} ${ENV_NAME} ${output_dir} 'Remove bbl state dir''" EXIT
+  trap "commit_bbl_state_dir ${bbl_state_env_repo_dir} ${ENV_NAME} ${output_dir} 'Remove bbl state dir'" EXIT
 
   (
     source_bbl_env "envs/${ENV_NAME}"
