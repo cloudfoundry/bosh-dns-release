72b924a8ad292a6b40fe2a3ccde7359736cbc097
Joshua Aresty <joshua.aresty@emc.com>
2019-01-04 15:36:59 -0800

    Use subshell to trigger outer trap
    [#162976693](https://www.pivotaltracker.com/story/show/162976693)
    
    Co-authored-by: Max Petersen <mpetersen@pivotal.io>

--- ci/tasks/test-stress/destroy-env.sh
+++ ci/tasks/test-stress/destroy-env.sh
@@ -5,9 +5,11 @@ main() {
   local bbl_state_env_repo_dir=$PWD/bbl-state
   trap "commit_bbl_state_dir ${bbl_state_env_repo_dir} ${BBL_STATE_DIR} ${output_dir} 'Remove bbl state dir'" EXIT
 
-  if source_bbl_env bbl-state/${BBL_STATE_DIR} && bosh env; then
-    clean_up_director docker
-  fi
+  (
+    if source_bbl_env bbl-state/${BBL_STATE_DIR} && bosh env; then
+      clean_up_director docker
+    fi
+  )
 
   pushd "bbl-state/${BBL_STATE_DIR}"
     bbl version
