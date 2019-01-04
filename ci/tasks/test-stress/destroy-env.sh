67828d749733dca12ea2ae0708d9be5b7aa97b2d
Joshua Aresty <joshua.aresty@emc.com>
2019-01-04 12:05:46 -0800

    Destroy test-stress bbl env even if create-env failed
    Co-authored-by: Max Petersen <mpetersen@pivotal.io>

--- ci/tasks/test-stress/destroy-env.sh
+++ ci/tasks/test-stress/destroy-env.sh
@@ -5,16 +5,14 @@ main() {
   local bbl_state_env_repo_dir=$PWD/bbl-state
   trap "commit_bbl_state_dir ${bbl_state_env_repo_dir} ${BBL_STATE_DIR} ${output_dir} 'Remove bbl state dir'" EXIT
 
-  (
-    source_bbl_env bbl-state/${BBL_STATE_DIR}
+  if source_bbl_env bbl-state/${BBL_STATE_DIR} && bosh env; then
     clean_up_director docker
-  )
+  fi
 
-  pushd bbl-state/${BBL_STATE_DIR}
-    bbl version
+  cd "bbl-state/${BBL_STATE_DIR}"
+  bbl version
 
-    bbl --debug destroy --no-confirm
-  popd
+  bbl --debug destroy --no-confirm
 }
 
 main
