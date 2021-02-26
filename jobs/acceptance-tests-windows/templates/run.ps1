c81343098613fca745fadff2ab7e533eebeab787
Joseph Palermo <jpalermo@pivotal.io>
2021-02-25 16:41:55 -0800

    Bring back acceptance-tests-windows suites property
    - Was used for testing with nameserver-disabled
    
    [#177085361]

--- jobs/acceptance-tests-windows/templates/run.ps1
+++ jobs/acceptance-tests-windows/templates/run.ps1
@@ -9,7 +9,7 @@ Copy-Item -recurse -force  "C:\var\vcap\packages\acceptance-tests-windows\src\bo
 Push-Location "${env:GOPATH}\src\bosh-dns"
 
 $env:LOCAL_IP_ADDRESS = "<%= spec.ip %>"
-go.exe run github.com/onsi/ginkgo/ginkgo -randomizeAllSpecs -randomizeSuites -race acceptance_tests/windows
+go.exe run github.com/onsi/ginkgo/ginkgo -randomizeAllSpecs -randomizeSuites -race acceptance_tests/windows/<%= p('suites') %>
 
 Pop-Location
 
