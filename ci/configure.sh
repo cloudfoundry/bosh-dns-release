395aafc48dd41e57bac485a510c68d4f83d2c66e
Joseph Palermo <jpalermo@pivotal.io>
2023-01-12 14:42:37 -0800

    Update ci pipeline with new credhub entries

--- ci/configure.sh
+++ ci/configure.sh
@@ -8,7 +8,4 @@ dir=$(dirname $0)
 
 fly -t ${CONCOURSE_TARGET:-bosh-ecosystem} \
   sp -p bosh-dns-release \
-  -c $dir/pipeline.yml \
-  -l <(lpass show --notes 'dns-release pipeline vars') \
-  -l <(lpass show --notes 'tracker-bot-story-delivery') \
-  -v "tracker_project_id=956238"
+  -c $dir/pipeline.yml
