e7e86af0bd050637d89ab9a0c53f92981fa3c8c5
Kenneth Lakin <klakin@pivotal.io>
2023-02-09 11:51:59 -0800

    Remove -race when running windows acceptance tests
    - CGO_ENABLED is required, but when we enable that, it means we then need gcc
    - Race detection isn't important when running acceptance tests
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- jobs/acceptance-tests-windows/templates/run.ps1
+++ jobs/acceptance-tests-windows/templates/run.ps1
@@ -9,8 +9,8 @@ Copy-Item -recurse -force  "C:\var\vcap\packages\acceptance-tests-windows\src\bo
 Push-Location "${env:GOPATH}\src\bosh-dns"
 
 $env:LOCAL_IP_ADDRESS = "<%= spec.ip %>"
-$env:CGO_ENABLED = "1"
-go.exe run github.com/onsi/ginkgo/ginkgo -randomizeAllSpecs -randomizeSuites -race acceptance_tests/windows/<%= p('suites') %>
+
+go.exe run github.com/onsi/ginkgo/ginkgo -randomizeAllSpecs -randomizeSuites acceptance_tests/windows/<%= p('suites') %>
 
 Pop-Location
 
