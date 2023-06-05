74f988867afbbf31557fd2ef014de5af358ec19f
Long Nguyen <nguyenlo@vmware.com>
2023-06-05 10:25:59 -0400

    Update to use google range

--- ci/assets/test-stress/terraform/google-routes.tf
+++ ci/assets/test-stress/terraform/google-routes.tf
@@ -2,7 +2,7 @@ resource "google_compute_route" "docker0" {
   name         = "docker0"
   dest_range   = "10.245.0.0/16"
   network      = google_compute_network.bbl-network.name
-  next_hop_ip  = "10.0.31.190"
+  next_hop_ip  = "10.0.255.190"
   tags         = ["docker0"]
   depends_on = [
     google_compute_subnetwork.bbl-subnet
@@ -12,7 +12,7 @@ resource "google_compute_route" "docker1" {
   name         = "docker1"
   dest_range   = "10.246.0.0/16"
   network      = google_compute_network.bbl-network.name
-  next_hop_ip  = "10.0.31.191"
+  next_hop_ip  = "10.0.255.191"
   tags         = ["docker1"]
   depends_on = [
     google_compute_subnetwork.bbl-subnet
@@ -22,7 +22,7 @@ resource "google_compute_route" "docker2" {
   name         = "docker2"
   dest_range   = "10.247.0.0/16"
   network      = google_compute_network.bbl-network.name
-  next_hop_ip  = "10.0.31.192"
+  next_hop_ip  = "10.0.255.192"
   tags         = ["docker2"]
   depends_on = [
     google_compute_subnetwork.bbl-subnet
@@ -32,7 +32,7 @@ resource "google_compute_route" "docker3" {
   name         = "docker3"
   dest_range   = "10.248.0.0/16"
   network      = google_compute_network.bbl-network.name
-  next_hop_ip  = "10.0.31.193"
+  next_hop_ip  = "10.0.255.193"
   tags         = ["docker3"]
   depends_on = [
     google_compute_subnetwork.bbl-subnet
@@ -42,7 +42,7 @@ resource "google_compute_route" "docker4" {
   name         = "docker4"
   dest_range   = "10.249.0.0/16"
   network      = google_compute_network.bbl-network.name
-  next_hop_ip  = "10.0.31.194"
+  next_hop_ip  = "10.0.255.194"
   tags         = ["docker4"]
   depends_on = [
     google_compute_subnetwork.bbl-subnet
@@ -52,7 +52,7 @@ resource "google_compute_route" "docker5" {
   name         = "docker5"
   dest_range   = "10.250.0.0/16"
   network      = google_compute_network.bbl-network.name
-  next_hop_ip  = "10.0.31.195"
+  next_hop_ip  = "10.0.255.195"
   tags         = ["docker5"]
   depends_on = [
     google_compute_subnetwork.bbl-subnet
@@ -62,7 +62,7 @@ resource "google_compute_route" "docker6" {
   name         = "docker6"
   dest_range   = "10.251.0.0/16"
   network      = google_compute_network.bbl-network.name
-  next_hop_ip  = "10.0.31.196"
+  next_hop_ip  = "10.0.255.196"
   tags         = ["docker6"]
   depends_on = [
     google_compute_subnetwork.bbl-subnet
@@ -72,7 +72,7 @@ resource "google_compute_route" "docker7" {
   name         = "docker7"
   dest_range   = "10.252.0.0/16"
   network      = google_compute_network.bbl-network.name
-  next_hop_ip  = "10.0.31.197"
+  next_hop_ip  = "10.0.255.197"
   tags         = ["docker7"]
   depends_on = [
     google_compute_subnetwork.bbl-subnet
@@ -82,7 +82,7 @@ resource "google_compute_route" "docker8" {
   name         = "docker8"
   dest_range   = "10.253.0.0/16"
   network      = google_compute_network.bbl-network.name
-  next_hop_ip  = "10.0.31.198"
+  next_hop_ip  = "10.0.255.198"
   tags         = ["docker8"]
   depends_on = [
     google_compute_subnetwork.bbl-subnet
@@ -92,7 +92,7 @@ resource "google_compute_route" "docker9" {
   name         = "docker9"
   dest_range   = "10.254.0.0/16"
   network      = google_compute_network.bbl-network.name
-  next_hop_ip  = "10.0.31.199"
+  next_hop_ip  = "10.0.255.199"
   tags         = ["docker9"]
   depends_on = [
     google_compute_subnetwork.bbl-subnet
