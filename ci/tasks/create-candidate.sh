7bd598d9c4b9111afc18cdd43347ec26ae77d58f
Brian Upton <brian.upton@broadcom.com>
2024-11-06 09:29:23 -0800

    Fix directory for candidate release

--- ci/tasks/create-candidate.sh
+++ ci/tasks/create-candidate.sh
@@ -6,4 +6,4 @@ export ROOT_PATH=$PWD
 
 cd bosh-dns-release
 
-bosh create-release --tarball=../release/bosh-dns-dev-release.tgz --timestamp-version --force
+bosh create-release --tarball=../candidate-release/bosh-dns-dev-release.tgz --timestamp-version --force
