8614d23ddfd30cb00709d76cc030e12881a45d85
Chris Selzo <chris.selzo@broadcom.com>
2025-07-08 14:58:54 -0700

    ci: try dropping instance profile for bosh director in test-stress
    Signed-off-by: Aram Price <aram.price@broadcom.com>
    Co-authored-by: Aram Price <aram.price@broadcom.com>

--- ci/assets/test-stress/director/create-director.sh
+++ ci/assets/test-stress/director/create-director.sh
@@ -7,7 +7,6 @@ bosh int \
   -o ${BBL_STATE_DIR}/bosh-deployment/jumpbox-user.yml \
   -o ${BBL_STATE_DIR}/bosh-deployment/uaa.yml \
   -o ${BBL_STATE_DIR}/bosh-deployment/credhub.yml \
-  -o ${BBL_STATE_DIR}/bosh-deployment/aws/iam-instance-profile.yml \
   -o ${BBL_STATE_DIR}/bosh-deployment/aws/cpi-assume-role-credentials.yml \
   -o ${BBL_STATE_DIR}/bbl-ops-files/aws/bosh-director-ephemeral-ip-ops.yml \
   -o ${TEST_STRESS_ASSETS}/director/ops/add-docker-cpi-release.yml \
