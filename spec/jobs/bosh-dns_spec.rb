325a4d0b47760b5926f823b1a28f41c64c77b2e0
Matthew Kocher <matthew.kocher@broadcom.com>
2026-06-10 11:58:44 -0700

    Replace chpst with setpriv in bosh-dns job scripts
    The runit package (which provides chpst) is removed from the Resolute
    Raccoon stemcell. Replace chpst -u vcap:vcap with setpriv
    --reuid=vcap --regid=vcap --clear-groups -- in both bosh_dns_ctl and
    bosh_dns_health_ctl, consistent with the pattern used in bat-release.

--- spec/jobs/bosh-dns_spec.rb
+++ spec/jobs/bosh-dns_spec.rb
@@ -6,4 +6,34 @@ describe 'bosh-dns' do
 
   it_behaves_like 'common config.json'
 
+  describe 'bin/bosh_dns_ctl' do
+    let(:template) { job.template('bin/bosh_dns_ctl') }
+    let(:rendered) { template.render({}) }
+
+    it 'does not reference chpst' do
+      expect(rendered).not_to include('chpst')
+    end
+
+    it 'uses setpriv to drop privileges to vcap' do
+      expect(rendered).to include('setpriv --reuid=vcap --regid=vcap --clear-groups --')
+    end
+
+    it 'does not use --no-new-privs because bosh-dns relies on cap_net_bind_service file capability for port 53' do
+      expect(rendered).not_to include('--no-new-privs')
+    end
+  end
+
+  describe 'bin/bosh_dns_health_ctl' do
+    let(:template) { job.template('bin/bosh_dns_health_ctl') }
+    let(:rendered) { template.render({}) }
+
+    it 'does not reference chpst' do
+      expect(rendered).not_to include('chpst')
+    end
+
+    it 'uses setpriv with --no-new-privs to drop privileges to vcap' do
+      expect(rendered).to include('setpriv --reuid=vcap --regid=vcap --clear-groups --no-new-privs --')
+    end
+  end
+
 end
