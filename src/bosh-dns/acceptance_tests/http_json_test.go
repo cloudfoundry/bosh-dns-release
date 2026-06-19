7a84dde978c58976bd5878518235274dcdff85f3
Matthew Kocher <matthew.kocher@broadcom.com>
2026-06-19 16:07:19 -0700

    tests: integration tests go over ssh now
    Integration tests used to configure bosh dns to listen on 0.0.0.0 which was not
    represenative of production and didn't test the systemd integration. Now we do
    all resolutions from inside the VM, testing the systemd integration when
    present.

--- src/bosh-dns/acceptance_tests/http_json_test.go
+++ src/bosh-dns/acceptance_tests/http_json_test.go
@@ -20,7 +20,7 @@ var _ = Describe("HTTP JSON Server integration", func() {
 		})
 
 		It("answers queries with the response from the http server", func() {
-			dnsResponse := helpers.Dig("app-id.internal.local.", firstInstance.IP)
+			dnsResponse := helpers.RemoteDig(firstInstance.Slug(), "app-id.internal.local.")
 			Expect(dnsResponse).To(gomegadns.HaveFlags("qr", "aa", "rd", "ra"))
 			Expect(dnsResponse.Answer).To(ConsistOf(
 				gomegadns.MatchResponse(gomegadns.Response{"ip": "192.168.0.1", "ttl": 0}),
@@ -28,7 +28,7 @@ var _ = Describe("HTTP JSON Server integration", func() {
 		})
 
 		It("configures Bosh DNS handlers by producing a dns handlers json file", func() {
-			dnsResponse := helpers.Dig("handler.internal.local.", firstInstance.IP)
+			dnsResponse := helpers.RemoteDig(firstInstance.Slug(), "handler.internal.local.")
 			Expect(dnsResponse).To(gomegadns.HaveFlags("qr", "aa", "rd", "ra"))
 			Expect(dnsResponse.Answer).To(ConsistOf(
 				gomegadns.MatchResponse(gomegadns.Response{"ip": "10.168.0.1", "ttl": 0}),
